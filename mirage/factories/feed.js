3cf27bdbf9e96aaec4de9ff38a6d12c87b23e98b
njpaz <nicole.j.paz@gmail.com>
2022-05-14 17:48:58 -0700

    Update Mirage data so it better reflects the data.

--- mirage/factories/feed.js
+++ mirage/factories/feed.js
@@ -1,13 +1,16 @@
-import { Factory, association } from 'miragejs';
+import { Factory } from 'miragejs';
 import faker from '@faker-js/faker';
 
 export default Factory.extend({
-  feedItems: association(),
-
   title() {
     return faker.random.words();
   },
   url() {
     return faker.internet.url();
-  }
+  },
+  afterCreate(feed, server) {
+    feed.update({
+      feedItems: server.createList('feed-item', 5),
+    });
+  },
 });
