1d347221f3392f17082490505aae8193d6523ab2
njpaz <nicole.j.paz@gmail.com>
2022-05-16 18:27:22 -0700

    Rename FeedItem to FeedView/Item.

--- app/components/feed-view/item.js
+++ app/components/feed-view/item.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
 
-export default class FeedItemComponent extends Component {
+export default class FeedViewItemComponent extends Component {
   constructor() {
     super(...arguments);
 
--- tests/integration/components/feed-view/item-test.js
+++ tests/integration/components/feed-view/item-test.js
@@ -3,19 +3,19 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | feed-item', function (hooks) {
+module('Integration | Component | feed-view/item', function (hooks) {
   setupRenderingTest(hooks);
 
-  hooks.beforeEach(function () {
+  test('it generates a unique element id and adds it to the page', async function (assert) {
+    assert.expect(4);
+
     this.set('item', {
       title: 'Some title',
       description: 'A description'
     });
-  });
+    this.set('selectVisibleIndex', () => {});
 
-  test('it generates a unique element id and adds it to the page', async function (assert) {
-    assert.expect(4);
-    await render(hbs`<FeedItem @item={{this.item}} />`);
+    await render(hbs`<FeedView::Item @item={{this.item}} @selectVisibleIndex={{this.selectVisibleIndex}} />`);
 
     let { id } = find('[data-id="item-description"]');
 
